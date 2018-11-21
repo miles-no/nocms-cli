19e097bf8ec95bebaed7211b5917be6f418caf07
mskutle <magne.skutle@gmail.com>
2018-11-21 21:13:25 +0100

    feat: added a couple of files to the web & config-api template that I had forgotten to add earlier.

--- template/root/containers/config-api/app/adapters/fragments.js
+++ template/root/containers/config-api/app/adapters/fragments.js
@@ -2,6 +2,7 @@ module.exports = function adapter(remoteConfig) {
   const config = {
     sites: remoteConfig.SITE_LIST,
     logConfig: remoteConfig.LOG_CONFIG,
+    pageService: remoteConfig.PAGE_SERVICE
   };
 
   const encrypted = [];
